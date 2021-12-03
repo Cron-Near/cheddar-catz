26be0347f3975edf9a6f39a0917489732b58f472
trevorjtclarke <trevorjtclarke@gmail.com>
2021-12-03 10:50:10 -0800

    remove unneeded proposal ids

--- dao/cheddar_dao_proposals_mainnet.sh
+++ dao/cheddar_dao_proposals_mainnet.sh
@@ -117,9 +117,7 @@ actions=(0 1 2 3 4 5 6 7 8 9)
 # # 2
 # actions=(10 11 12 13 14 15 16 17 18 19)
 # # 3
-# actions=(20 21 22 23 24 25 26 27 28 29)
-# # 4
-# actions=(30 31 32 33 34 35)
+# actions=(20 21 22 23 24 25 26 27 28)
 for (( d=0; d<=${#actions[@]} - 1; d++ ))
 do
   action_type="VoteApprove"
